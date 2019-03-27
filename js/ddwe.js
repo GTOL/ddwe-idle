2c0331c1214972f51e5eb21740edcb00f1b41569
GTOL <gt531593545@126.com>
2019-03-27 16:18:59 +0800

    Finish version 1

--- js/ddwe.js
+++ js/ddwe.js
@@ -1,11 +1,18 @@
 class ddwe {
-	constructor(level) {
-		this.level = level;
+	constructor(price, multiplier, production) {
+		this.price = price;
+		this.multiplier = multiplier;
+		this.production = production
+	}
+
+	amount = 0;
+
+	buy() {
+		this.amount++;
+		this.price *= this.multiplier;
 	}
 }
 
-var ddwe1 = new ddwe(1);
+var heraldDdwe = new ddwe(1, 1.2, 0.1);
 
-
-
-console.log(ddwe1);
+console.log(heraldDdwe);
